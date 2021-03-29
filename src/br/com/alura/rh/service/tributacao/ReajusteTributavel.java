ae85c6cb5bdda78623f6fa8324ac93fb8ee5bfae
Gabriel <gabriel.romano.3194@hotmail.com>
2021-03-29 15:40:06 -0300

    anotações

--- src/br/com/alura/rh/service/tributacao/ReajusteTributavel.java
+++ src/br/com/alura/rh/service/tributacao/ReajusteTributavel.java
@@ -2,7 +2,7 @@ package br.com.alura.rh.service.tributacao;
 
 import java.math.BigDecimal;
 
-public interface ReajusteTributavel extends Reajuste {
+public interface ReajusteTributavel extends Reajuste { //herda de reajuste, é filho de reajuste, pq ReajusteTributavel eh um tipo de reajuste
 	
 	BigDecimal valorImpostoDeRenda();
 }
